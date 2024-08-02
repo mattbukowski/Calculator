cf5ca06c6545f9ac16c041493b56e236f9bf793e
mattbukowski <mattbukowski98@gmail.com>
2024-08-02 22:20:30 +0200

    Add styled template for calculator

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container")
+container.style.cssText = `width: 800px; height: 800px;`
+const results = document.querySelector(".results")
+const fragment = document.createDocumentFragment()
+
+function adding () {
+    
+}
